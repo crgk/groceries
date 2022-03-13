695d97156af74678ef58198d2ea0c805d0327b3d
Chad Knight <chad@knightleow.com>
2022-03-13 07:44:29 -1000

    favicon

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,11 @@
 import styles from './Footer.module.css'
 
 export default function Footer() {
-  return (
-    <>
-      <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
-      </footer>
-    </>
-  )
-}
+	return (
+	<>
+		<footer className={styles.footer}>
+			Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+			<a href="https://www.flaticon.com/free-icons/basket-shop" title="basket shop icons">Basket shop icons created by pictranoosa - Flaticon</a>
+		</footer>
+	</>
+)}
