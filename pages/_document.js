eafa357ac18dda3d509dd88e921120ee27426564
Chad Knight <chad@knightleow.com>
2022-04-17 17:25:56 -1000

    take another pass at apple-flavored meta

--- pages/_document.js
+++ pages/_document.js
@@ -6,9 +6,13 @@ function Document() {
 		<Html lang="en">
 			<Head>
 				<meta name="theme-color" content={theme.palette.primary.main} />
-				<meta name="mobile-web-app-capable" content="yes" />
 				<link rel="icon" href="/icons/basket-check-icon-white.png" />
+
+				<meta name="apple-mobile-web-app-capable" content="yes" />
+				<meta name="apple-mobile-web-app-title" content="Essentials" />
+				<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
 				<link rel="apple-touch-icon" href="/icons/basket-check-150x150-white.png" />
+
 				<link rel="webmanifest" href="manifest.webmanifest" />
 			</Head>
 			<body>
